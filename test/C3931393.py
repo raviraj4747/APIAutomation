4503ea7ac3b08ea844930f02d71002896bd69147
ASAHI-GH22010 <ravirajsinh.solanki@asahigroup-holdings.com>
2024-06-28 14:11:15 +0900

    new file added

--- test/C3931393.py
+++ test/C3931393.py
@@ -1,10 +1,9 @@
 import requests
 
-def test_dummy_api():
-    # Dummy API endpoint
-    url = 'https://www.google.com'
-    response = requests.get(url)
-    assert response.status_code == 200
+class TestLogin:
 
-if __name__ == '__main__':
-    test_dummy_api()
+    def test_dummy_api(self):
+        # Dummy API endpoint
+        url = 'https://www.google.com'
+        response = requests.get(url)
+        assert response.status_code == 200, f"Expected status code 200, but got {response.status_code}"
