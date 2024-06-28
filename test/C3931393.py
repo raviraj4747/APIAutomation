8633a42b8825f2a3d512f8e06e605f4b93c461cd
ASAHI-GH22010 <ravirajsinh.solanki@asahigroup-holdings.com>
2024-06-28 13:25:40 +0900

    new file added

--- test/C3931393.py
+++ test/C3931393.py
@@ -1,32 +1,10 @@
-import random
-import string
-from test.Utilities.CommonActions import CommonActions
-from test.conftest import BaseTest
-from test.Keywords.ENT.Login import TestLogIn
-from test.Pages.LogInPage import LogInPage
-from test.Profiles.ENTProfile import ENTProfile
-from test.Utilities.TestObjectAction import ObjectActions
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as ec
-from selenium.webdriver.support.wait import WebDriverWait
-import time
+import requests
 
-testcase_name = "C3931393"
+def test_dummy_api():
+    # Dummy API endpoint
+    url = 'https://www.google.com'
+    response = requests.get(url)
+    assert response.status_code == 200
 
-
-class TestLogin(BaseTest):
-
-    def test_C3931393(self):
-
-        try:
-            # login into application1`
-            TestLogIn.login_into_application(self.driver, ENTProfile.USERNAME1, ENTProfile.PASSWORD)
-            time.sleep(60)
-            ObjectActions.click_object(self.driver,LogInPage.Contact_us)
-            ObjectActions.click_object(self.driver, LogInPage.Contact_group_holding)
-
-        except:
-            CommonActions.mark_fail(self.driver, testcase_name)
-
-        else:
-            CommonActions.mark_pass(self.driver, testcase_name)
+if __name__ == '__main__':
+    test_dummy_api()
