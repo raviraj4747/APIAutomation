7c9fbbbdce6f2c353c39e4e48893b117b1dafc06
ASAHI-GH22010 <ravirajsinh.solanki@asahigroup-holdings.com>
2024-06-28 19:24:14 +0900

    new file added

---
+++ test/C3931395.py
@@ -0,0 +1,25 @@
+import random
+import string
+from test.Utilities.CommonActions import CommonActions
+from test.conftest import BaseTest
+from test.Keywords.ENT.Login import TestLogIn
+from test.Pages.LogInPage import LogInPage
+from test.Profiles.ENTProfile import ENTProfile
+from test.Utilities.TestObjectAction import ObjectActions
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.support.wait import WebDriverWait
+import requests
+import time
+
+testcase_name = "C3931395"
+
+
+class TestLogin():
+
+    def test_C3931395(self):
+
+            # login into application1`
+            url = 'https://www.google.com'
+            response = requests.get(url)
+            assert response.status_code == 200, f"Expected status code 200, but got {response.status_code}"
